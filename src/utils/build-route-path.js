736899c6d3067452c24cd7a4749894375d1be9b6
viitones <vitonesmartins@gmail.com>
2025-04-08 07:53:55 -0300

    testing regex

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,12 @@
+
+
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g
+  const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)')
+
+  // const pathRegex = new RegExp(`^${pathWithParams}`)
+  const test = /\/users\/([a-z0-9-_]+)/
+  console.log(test);
+  
+  // return pathRegex
+}
